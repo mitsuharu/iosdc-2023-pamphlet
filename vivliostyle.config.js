ea604ba21bd6854f1a2dd073d3a7c1396615db27
Mitsuharu Emoto <mthr1982@gmail.com>
2023-06-05 22:23:08 +0900

    add vivliostyle.config.js

---
+++ vivliostyle.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'mybook',
+    author: '江本光晴 <mthr1982@gmail.com>',
+    language: 'ja',
+    theme: '@vivliostyle/theme-techbook',
+    entry: [
+      'index.md',
+    ],
+    entryContext: './manuscripts',
+    output: [
+      './output/output.pdf',
+    ],
+    workspaceDir: '.vivliostyle',
+    toc: false
+    // cover: './cover.png',
+    // vfm: {
+    //   hardLineBreaks: true,
+    //   disableFormatHtml: true,
+    // },
+  }
